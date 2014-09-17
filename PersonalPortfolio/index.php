1256b61dba648ed28c820aecb8f564b9665203fe
jamesisninja <jamesisninja@hotmail.com>
2014-09-17 11:54:25 -0400

    WebsiteV2

---
+++ PersonalPortfolio/index.php
@@ -0,0 +1,37 @@
+﻿ <?PHP
+    include ("header.html");
+ ?>
+<nav id="nav-1">
+
+    <a class="link-1-Current" href="#">Home</a>
+    <a class="link-1" href="#">About</a>
+    <a class="link-1" href="#">Contact</a>
+    <a class="link-1" href="#">Shop</a>
+    <a class="link-1" href="#">Shop</a>
+    <a class="link-1" href="#">Shop</a>
+</nav>
+    <h1 class="welcomeStatement">{Hello to You!}</h1>
+    <p class="name">James White</p>
+    <h2 class="personalTagline">Work hard? Work Smart.</h2>
+<div id="slider">
+    <a href="#" class="control_next">></a>
+    <a href="#" class="control_prev"><</a>
+    <ul>
+        <li>SLIDE 1</li>
+        <li style="background: #aaa;">SLIDE 2</li>
+        <li>SLIDE 3</li>
+        <li style="background: #aaa;">SLIDE 4</li>
+    </ul>
+</div>
+
+    <p class="content"> lorem ipsum dolor sit amet, consectetur adipiscing elit. quisque nec placerat magna. phasellus nec ante venenatis, sollicitudin urna vitae, finibus ante. donec semper aliquet nulla, ut accumsan urna cursus ut. integer congue ipsum eu mauris accumsan, vitae cursus nunc pulvinar. nulla molestie, sem ut sodales lobortis, nibh dolor consectetur massa, id dignissim diam est vel sapien. pellentesque a feugiat velit. etiam sit amet tellus sed arcu venenatis aliquam in quis lectus. morbi id mattis nisl. etiam non ligula vitae tortor euismod ultrices a vel purus. nunc pellentesque luctus lacinia. aenean nibh sapien, pellentesque eget faucibus eleifend, varius vulputate augue. nullam vitae ipsum ut urna cursus sollicitudin. donec sit amet imperdiet nibh. vestibulum id nisi a odio rhoncus auctor id nec odio. donec cursus elementum neque non aliquam.
+       maecenas a tortor non eros accumsan dictum vel eget ipsum. suspendisse elit sem, porttitor in ante ac, interdum pulvinar mauris. in vel magna pulvinar, sollicitudin sapien ut, tincidunt magna. ut dolor lectus, ornare vitae varius ut, eleifend sed nisi. donec tristique eleifend augue a aliquet. aenean sit amet elit pretium, volutpat urna a, molestie neque. vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; duis porta enim at mollis tempus. integer in rhoncus quam. suspendisse faucibus sapien vitae libero laoreet tempus. vestibulum vel turpis tortor. donec in orci dictum urna viverra laoreet. sed maximus justo et massa congue facilisis. cras mollis felis sed mollis euismod. quisque vestibulum ex quis nisl sollicitudin sodales. aenean accumsan sapien eget orci scelerisque malesuada.
+       suspendisse dui magna, posuere eget tempor ac, iaculis vel erat. sed luctus lacinia quam. aliquam porta in lacus vel iaculis. sed non dictum ipsum. duis sit amet tortor nec felis porttitor malesuada. etiam gravida semper quam, eget elementum nisi elementum ut. mauris sagittis lacus metus, non sollicitudin nunc vestibulum vel. etiam ultrices augue eu quam commodo, sit amet sollicitudin dolor blandit.
+       donec et pharetra lorem. cras luctus sed est dapibus volutpat. vestibulum lectus arcu, interdum id ex sit amet, consequat lobortis purus. pellentesque ut mi in mauris mattis sollicitudin. mauris suscipit nibh leo, ut tempus mauris placerat vitae. phasellus tempus, leo at molestie sagittis, enim nunc laoreet ipsum, quis finibus turpis sapien at lorem. nunc fermentum ut nunc vitae pretium. sed sit amet quam ac ex rhoncus mattis. curabitur eget consequat ex, vel fringilla odio. in at ligula at nulla vulputate feugiat sit amet nec diam. vivamus in sem augue. nullam nec leo eget diam lobortis ultrices. aliquam consequat auctor ligula, ac sollicitudin nunc laoreet a. nunc eget nisl ullamcorper, scelerisque urna ut, sollicitudin nisl.
+       duis bibendum ante quis mi tincidunt, ac egestas diam sagittis. integer quis est non ipsum iaculis facilisis. vivamus pulvinar eleifend vestibulum. fusce eu imperdiet neque. sed ac tellus quis dui sodales luctus. cras hendrerit facilisis luctus. fusce ultrices et tellus vel fermentum. pellentesque non ultricies dui, in sagittis risus. cras porttitor mauris sem, quis scelerisque velit feugiat a. cras non ante est. nulla tristique ultricies dui, in dignissim felis rhoncus ac. integer sed vehicula velit. proin eget suscipit dolor, at vehicula nisl. etiam eu viverra nulla, id vehicula nibh.
+       </p>
+
+
+ <?PHP
+    include ("footer.html");
+ ?>  
